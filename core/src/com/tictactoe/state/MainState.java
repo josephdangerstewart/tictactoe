da9140057227e2bc7c27972eda1218b08787b9ae
Joseph Stewart <joseph.william.stewart@biola.edu>
2017-09-24 15:44:19 -0700

    Renamed Packages

---
+++ core/src/com/tictactoe/state/MainState.java
@@ -0,0 +1,14 @@
+package com.tictactoe.state;
+
+import com.tictactoe.state.meta.State;
+
+/**
+ * Created by josephstewart on 9/24/17.
+ */
+public class MainState extends State {
+
+    public MainState(String backgroundName) {
+        super(backgroundName);
+    }
+
+}
---
+++ html/src/com/tictactoe/main/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.tictactoe.main.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.tictactoe.main.TicTacToeMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new TicTacToeMain();
+        }
+}
